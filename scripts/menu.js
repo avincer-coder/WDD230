27d97b77ec40290cc7c55b953d214df0d08446c0
Mario Eduardo Garcia Salazar <marioeduardo.garciasalazar@gmail.com>
2025-01-21 21:17:23 -0600

    dark mode

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,33 @@
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
+const aLinks = document.querySelectorAll('.weekLinks');
 
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
+});
+
+console.log(aLinks)
+// aLinks.color = "red"
+
+const modeButton = document.querySelector("#darkMode");
+const body = document.querySelector("body");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		body.style.background = "#000";
+		body.style.color = "#fff";
+		modeButton.textContent = "🔆";
+		aLinks.forEach((link)=>{
+			link.style.color = "#fff"
+		})
+
+	} else {
+		body.style.background = "#eee";
+		body.style.color = "#000";
+		modeButton.textContent = "🕶️";
+		aLinks.forEach((link)=>{
+			link.style.color = "#000"
+		})
+	}
 });
